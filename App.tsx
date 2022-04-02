2cecd3b2b3f50fc325d763bf2a38b13da6137801
MochKrzysztof <krzysmoch.programs@gmail.com>
2022-04-02 20:05:25 +0200

    add ignore for redux warning about serialization took a long time

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { StyleSheet } from 'react-native';
+import { LogBox, StyleSheet } from 'react-native';
 import { Provider } from 'react-redux';
 import store from './src/redux/store';
 import MainScreen from './src/screens/MainScreen';
 
 const App = () => {
+  // Ignores redux warning about serialization took a long time
+  LogBox.ignoreLogs(['SerializableStateInvariantMiddleware']);
+
   return (
     <GestureHandlerRootView style={StyleSheet.absoluteFill}>
       <Provider store={store}>
