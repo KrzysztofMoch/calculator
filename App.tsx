68850eeeef5e9610302d0aa0fd56bd7e664284c7
MochKrzysztof <krzysmoch.programs@gmail.com>
2022-04-02 19:57:24 +0200

    install redux

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import MainScreen from './src/screens/MainScreen';
 import { StyleSheet } from 'react-native';
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
+import MainScreen from './src/screens/MainScreen';
 
 const App = () => {
   return (
     <GestureHandlerRootView style={StyleSheet.absoluteFill}>
-      <MainScreen />
+      <Provider store={store}>
+        <MainScreen />
+      </Provider>
     </GestureHandlerRootView>
   );
 };
